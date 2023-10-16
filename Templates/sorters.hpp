1d7d5fc57aa9422210f6f3e43e1f3ead3488b194
Peter Chapin <spicacality@kelseymountain.org>
2023-10-16 13:58:02 -0400

    Enhanced the bubble sort algorithm example.

---
+++ Templates/sorters.hpp
@@ -0,0 +1,189 @@
+
+#ifndef SORTERS_HPP
+#define SORTERS_HPP
+
+#include <algorithm>    // Needed for std::swap.
+#include <iterator>     // Needed for std::iterator_traits.
+#include <functional>   // Needed for std::less.
+
+namespace vtsu {
+
+    // A non-generic version of the bubble_sort algorithm.
+    
+    // NOTE: The body of this function should really be in a .cpp file. For this reason, it is
+    // compiled out here.
+    #ifdef NEVER
+    void bubble_sort_1( int *first, int *last )
+    {
+        if( first == last ) return;
+
+        bool done = false;
+        while( !done ) {
+            done = true;
+
+            int *p1 = first;
+            int *p2 = first + 1;
+
+            while( p2 != last ) {
+                if( *p2 < *p1 ) {
+                    std::swap( *p1, *p2 );
+                    done = false;
+                }
+                ++p1;
+                ++p2;
+            }
+        }
+    
+    }
+    #endif
+
+
+    // A generic version that allows sorting of any array with an element type has an
+    // `operator<`. This version assumes the sequence to be sorted is an array. We will further
+    // generalize this below.
+
+    template<typename T>
+    void bubble_sort_2( T *first, T *last )
+    {
+        if( first == last ) return;
+
+        bool done = false;
+        while( !done ) {
+            done = true;
+
+            T *p1 = first;
+            T *p2 = first + 1;
+
+            while( p2 != last ) {
+                if( *p2 < *p1 ) {
+                    std::swap( *p1, *p2 );
+                    done = false;
+                }
+                ++p1;
+                ++p2;
+            }
+        }
+    
+    }
+
+
+    // This version generalizes the sorting algorithm to work on any sequence of elements that
+    // supports forward iterators. Note that the name of the type parameter, `ForwardInteger`,
+    // is not really significant. It only serves as documentation. The fact that only forward
+    // iterators are required, and not some more powerful iterator type, is manifest in the
+    // operations that are used on objects of type `ForwardIterator`.
+
+    template<typename ForwardIterator>
+    void bubble_sort_3( ForwardIterator first, ForwardIterator last )
+    {
+        if( first == last ) return;
+
+        bool done = false;
+        while( !done ) {
+            done = true;
+
+            ForwardIterator p1 = first;
+            ForwardIterator p2 = first;
+            ++p2;
+            
+            while( p2 != last ) {
+                if( *p2 < *p1 ) {
+                    std::swap( *p1, *p2 );
+                    done = false;
+                }
+                ++p1;
+                ++p2;
+            }
+        }
+        
+    }
+
+
+    // This version further generalizes the algorithm to allow the use of a user-supplied
+    // comparison function. This is useful when the elements of the sequence to be sorted do not
+    // have an `operator<` defined for them or when the user wants to sort the elements in some
+    // order other than ascending. 
+
+    template<typename ForwardIterator, typename Comparison>
+    void bubble_sort( ForwardIterator first, ForwardIterator last, Comparison compare )
+    {
+        // We don't want to do 'using namespace std' in a header file. However, for reasons
+        // described below, we also don't want to qualify the use of swap with std::. Including
+        // a using declaration here (not a using directive!) gives us the effect we want.
+        //
+        // Note that the scope of the using declaration is limited to the function body. 
+        using std::swap;
+
+        if( first == last ) return;
+
+        bool done = false;
+        while( !done ) {
+            done = true;
+
+            ForwardIterator p1 = first;
+            ForwardIterator p2 = first;
+            ++p2;
+
+            while( p2 != last ) {
+
+                // The comparison object is used to define the sort order. It returns `true` if
+                // the first argument "comes before" the second argument in the desired order.
+                // It is up to the user to provide a suitable comparison object.
+                if( compare( *p2, *p1 ) ) {
+
+                    // Sequence elements are swapped here using, probably, the standard library
+                    // swap template, due to the using directive above. However, if the type
+                    // being swapped has it's own specialized swap function, it will be used
+                    // instead, because of overload resolution (an explicitly declared function
+                    // is used in preference to specializing a template). Many types can
+                    // implement swap much more efficiently than repeated copying, which is what
+                    // `std::swap` does, so taking advantage of their specialized swap functions
+                    // is very desirable.
+                    swap( *p1, *p2 );
+                    done = false;
+                }
+                ++p1;
+                ++p2;
+            }
+        }
+    }
+
+    // The following version of bubble_sort is a wrapper around the previous version. It
+    // provides a default comparison object, which is the standard library `less` function
+    // object. This allows the user to call bubble_sort with only two arguments, provided the
+    // user is content to sort in ascending order and the type `T` has a suitable `operator<`.
+    
+    template<typename ForwardIterator>
+    inline void bubble_sort( ForwardIterator first, ForwardIterator last )
+    {
+        // Note that `std::less` has no data members. It only has an overloaded `operator()`
+        // that allows an object of type `std::less` to be treated like a function. In particular:
+        //
+        //    std::less<int> less_than;
+        //    bool result = less_than( 1, 2 );  // Really less_than.operator()( 1, 2 );
+        //
+        // Here `result` is `true` because `1` is less than `2`.
+        //
+        // In the code below, the comparison object is constructed in place using an explicit
+        // call to the constructor. The resulting comparison object is then passed into the
+        // previous `bubble_sort`. Because of the magic of inline expansion, the compiler will
+        // generate highly efficient code for all of this.
+        bubble_sort( first, last,
+            std::less<typename std::iterator_traits<ForwardIterator>::value_type>{ } );
+    }
+
+
+    // The C way of creating a generic sorting function is as follows. Here the array is
+    // represented as a blob of raw memory with `array_size` elements, each `element_size`
+    // bytes. Internally, the function computes the location of each element "manually" and uses
+    // the provided comparison function to find their ordering. Such functions tend to be less
+    // efficient than the code above because of the overhead of calling the comparison function
+    // indirectly via a pointer. The C compiler can't inline expand the body of the comparison
+    // function because it doesn't know that at compile-time.
+    //
+    void bubble_sort(
+        void *first, std::size_t array_size, std::size_t element_size, bool (*compare)(void *, void *) );
+
+}
+
+#endif
--- Templates/sorters_demo.cpp
+++ Templates/sorters_demo.cpp
@@ -1,21 +1,38 @@
 
 #include <algorithm>
 #include <cstdlib>
+#include <forward_list>
 #include <functional>
 #include <iostream>
 #include <list>
 #include <string>
 #include <vector>
 
-#include "sorters_2.hpp"
+#include "sorters.hpp"
 
 using namespace std;
 
+// There are various ways one can define comparison objects. Because the compiler will
+// specialize the `bubble_sort_4` template for any comparison type, there is a lot of
+// flexibility. Here are some examples.
+
+// An ordinary function will work. The name of this function, `my_ordering` is taken to have
+// type `bool (*)(int, int)` (that is, "pointer to function taking two ints and returning
+// bool"). Since it allows for the application of `()` the name of this function will serve as a
+// comparison object for the `bubble_sort_4` template. The type `Comparison` in the template
+// will be specialized as `bool (*)(int, int)`.
+//
 bool my_ordering( int x, int y )
 {
-    return x > y;
+    return x > y;  // `x` "comes before" `y` if `x` is greater than `y`.
 }
 
+// Another approach is to create a structure with an overloaded `operator()`. That operator
+// could be defined inline (if it is simple) for better performance. Here we define it outside
+// the structure for clarity. When calling `bubble_sort_4` with an object of type
+// `MyOtherOrdering` the type `Comparison` in the template will be specialized as
+// `MyOtherOrdering`.
+//
 struct MyOtherOrdering {
     bool operator()( int x, int y );
 };
@@ -25,6 +42,9 @@ bool MyOtherOrdering::operator()( int x, int y )
 {
     bool xIsEven = (x % 2) == 0;
     bool yIsEven = (y % 2) == 0;
+
+    // All even numbers come before any odd numbers. Within the even numbers the sort order is
+    // ascending. Within the odd numbers the sort order is descending.
 
     if( xIsEven && yIsEven) {
         return x < y;
@@ -40,82 +60,105 @@ bool MyOtherOrdering::operator()( int x, int y )
     }
 }
 
-struct less {
-    bool operator()( int x, int y ) { return x < y; }
+// This is what `std::less<int>` looks like.
+struct my_less {
+    inline bool operator()( int x, int y ) { return x < y; }
 };
 
 
 int main( )
 {
+    // Let's create a comparison object here (which we could reuse, etc., if we wanted).
     MyOtherOrdering strange_order;
     
-    // Declare various entities that are sequences of things.
-    int array1[] = { 4, 6, 1, 7, 3, 9 };
-    std::string array2[] = { "hello", "world", "zebra", "apple" };
-    std::vector<int> my_vector = { 4, 6, 1, 7, 3, 9 };
-    std::list<int> my_list = { 4, 6, 1, 7, 3, 9 };
+    // A couple of different array types:
+    int array1[] = { 6, 1, 4, 7, 3, 9 };
+    std::string array2[] = { "hello", "world", "zebra", "apple", "fizzle", "sticks" };
 
-    // Sort them all and print them out. The compiler creates a fresh instantiation of the
-    // bubble_sort template for each data type required. Since both ints and strings have a
-    // suitable operator< defined, the instantiation of bubble_sort succeeds in every case.
+    // From the STL:
+    std::vector<int> my_vector = { 6, 1, 4, 7, 3, 9 };
+    std::list<int> my_list = { 6, 1, 4, 7, 3, 9 };
+    std::forward_list<int> my_forward_list = { 6, 1, 4, 7, 3, 9 };
+
+    // Sort them all and print them out. The compiler creates a fresh specialization of the
+    // `bubble_sort` template for each data type required. Since both ints and strings have a
+    // suitable `operator<` defined, the specialization of `bubble_sort` succeeds in every case.
     // Notice that the various library containers also have suitable iterator types defined (and
-    // all are at least as powerful as forward iterators).
+    // all are at least as powerful as forward iterators), so we can sort containers of various
+    // kinds.
 
     cout << "\nSorting array1..." << endl;
     // vtsu::bubble_sort( array1, array1 + 6 );
+    // C++ 2011 and beyond provide std::begin and std::end as an alternative to the above. They
+    // allow you to get the bounds on a container in a way that unifies the STL containers and
+    // ordinary arrays.
     vtsu::bubble_sort( begin( array1 ), end( array1 ) );
 
     for( int element : array1 ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
-    cout << "\nSorting array1 in a custom order..." << endl;
-    // vtsu::bubble_sort( array1, array1 + 6, my_ordering );
+    cout << "\nSorting array1 in descending order..." << endl;
+    // vtsu::bubble_sort_4( array1, array1 + 6, my_ordering );
     vtsu::bubble_sort( begin( array1 ), end( array1 ), my_ordering );
+    // One could also use `std::greater<int>( )` here.
 
     for( int element : array1 ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
     cout << "\nSorting array1 in a weird order..." << endl;
     // vtsu::bubble_sort( array1, array1 + 6, strange_order );
     vtsu::bubble_sort( begin( array1 ), end( array1 ), strange_order );
 
     for( int element : array1 ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
-    cout << "\nSorting array2..." << endl;
+    cout << "\nSorting array2 (an array of strings)..." << endl;
     // vtsu::bubble_sort( array2, array2 + 4 );
     vtsu::bubble_sort( begin( array2 ), end( array2 ) );
 
     for( const string &element : array2 ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
     cout << "\nSorting my_vector..." << endl;
     // vtsu::bubble_sort( my_vector.begin(), my_vector.end() );
     vtsu::bubble_sort( begin( my_vector ), end( my_vector ) );
 
     for( int element : my_vector ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
     cout << "\nSorting my_list..." << endl;
     // vtsu::bubble_sort( my_list.begin(), my_list.end() );
     vtsu::bubble_sort( begin( my_list ), end( my_list ) );
 
     for( int element : my_list ) {
-        cout << element << endl;
+        cout << element << " ";
     }
+    cout << endl;
 
-    cout << "\nSorting array1 in descending order..." << endl;
-    // vtsu::bubble_sort( array1, array1 + 6, std::greater<int>( ) );
-    vtsu::bubble_sort( begin( array1 ), end( array1 ), std::greater<int>( ) );
+    // Forward lists are interesting because they *only* provide forward iterators. However, the
+    // bubble sort algorithm is fine with that. Most sorting algorithms need more powerful
+    // iterators, and thus can't be used with forward lists. For example, the `std::sort`
+    // template requires random access iterators and thus can only work on arrays, vectors, and
+    // deques (or any user-defined container that provides random access iterators).
 
-    for( int element : array1 ) {
-        cout << element << endl;
+    cout << "\nSorting my_forward_list..." << endl;
+    // vtsu::bubble_sort( my_forward_list.begin(), my_forward_list.end() );
+    vtsu::bubble_sort( begin( my_forward_list ), end( my_forward_list ) );
+
+    for( int element : my_forward_list ) {
+        cout << element << " ";
     }
+    cout << endl;
 
     return EXIT_SUCCESS;
 }
