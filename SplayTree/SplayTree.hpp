57b8e3071cd3c417b81656400a6aec2668d3080f
Peter Chapin <spicacality@kelseymountain.org>
2023-11-03 14:46:54 -0400

    More work on SplayTree.

--- SplayTree/SplayTree.hpp
+++ SplayTree/SplayTree.hpp
@@ -10,6 +10,8 @@
 #include <functional>
 #include <initializer_list>
 #include <memory>
+#include <sstream>
+#include <vector>
 
 namespace spica {
 
@@ -80,6 +82,23 @@ namespace spica {
         size_type erase( const T &value );
         iterator erase( iterator position );
 
+        // Testing/Debugging
+        // -----------------
+        class InconsistentStructure : public std::logic_error {
+        public:
+            InconsistentStructure( const std::string &message ) :
+                std::logic_error( "Inconsistent SplayTree structure: " + message )
+                { }
+        };
+        void check_structure( ) const;
+
+        struct DumpItem {
+            size_type depth;
+            T data;
+        };
+        using DumpResult = std::vector<DumpItem>;
+        DumpResult dump( ) const;
+
     private:
         struct Node {
             T data;
@@ -96,6 +115,11 @@ namespace spica {
         NodePointer root;
         StrictWeakOrdering compare;
         size_type node_count;
+
+        // Testing/Debugging
+        // -----------------
+        size_type traverse_check( NodePointer p ) const;
+        std::vector<DumpItem> traverse_dump( NodePointer p, size_type depth ) const;
     };
 
     // Implementation
@@ -160,6 +184,95 @@ namespace spica {
         iterator saved{ *this };
         --( *this );
         return saved;
+    }
+
+    // Testing/Debugging
+    // -----------------
+
+    template<typename T, typename StrictWeakOrdering>
+    bool operator==( const typename SplayTree<T, StrictWeakOrdering>::DumpItem &lhs,
+                     const typename SplayTree<T, StrictWeakOrdering>::DumpItem &rhs )
+    {
+        return lhs.depth == rhs.depth && lhs.data == rhs.data;
+    }
+
+    template<typename T, typename StrictWeakOrdering>
+    void SplayTree<T, StrictWeakOrdering>::check_structure( ) const
+    {
+        if( root == nullptr && node_count == 0 ) return;
+        if( root == nullptr && node_count != 0 )
+            throw InconsistentStructure( "Non-zero node count with a null root" );
+        // The root cannot be null beyond this point.
+
+        if( root->parent != nullptr )
+            throw InconsistentStructure( "Root has a non-null parent" );
+
+        // Explore the tree structure and count the number of nodes.
+        size_type actual_count = traverse_check( root );
+        if( node_count != actual_count ) {
+            std::ostringstream formatter;
+            formatter << "node_count (" << node_count << ") != actual_count (" << actual_count << ")";
+            throw InconsistentStructure( formatter.str( ) );
+        }
+    }
+
+    template<typename T, typename StrictWeakOrdering>
+    typename SplayTree<T, StrictWeakOrdering>::size_type
+        SplayTree<T, StrictWeakOrdering>::traverse_check( NodePointer p ) const
+    {
+        size_type subtree_count = 0;
+        if( p != nullptr ) {
+            subtree_count++;
+
+            if( p->left != nullptr ) {
+                // Note that this check is incomplete. *Everything* in the left subtree should
+                // come before p->data. This only verifies that the root of the left subtree
+                // satisfies the invariant. Conceivably, nodes to the right of the left child
+                // should actually be to p's right. That won't be caught here.
+                //
+                if( !compare( p->left->data, p->data ) ) {
+                    throw InconsistentStructure( "Left child out of order" );
+                }
+                if( p->left->parent != p ) {
+                    throw InconsistentStructure( "Left child has bad parent" );
+                }
+                subtree_count += traverse_check( p->left );
+            }
+            if( p->right != nullptr ) {
+                // See comment above. A mirror issue exists here.
+                //
+                if( !compare( p->data, p->left->data ) ) {
+                    throw InconsistentStructure( "Right child out of order" );
+                }
+                if( p->right->parent != p ) {
+                    throw InconsistentStructure( "Right child has bad parent" );
+                }
+                subtree_count += traverse_check( p->right );
+            }
+        }
+        return subtree_count;
+    }
+
+    template<typename T, typename StrictWeakOrdering>
+    std::vector<typename SplayTree<T, StrictWeakOrdering>::DumpItem>
+        SplayTree<T, StrictWeakOrdering>::dump( ) const
+    {
+        return traverse_dump( root, 0 );
+    }
+
+    template<typename T, typename StrictWeakOrdering>
+    typename SplayTree<T, StrictWeakOrdering>::DumpResult
+        SplayTree<T, StrictWeakOrdering>::traverse_dump( NodePointer p, size_type depth ) const
+    {
+        DumpResult result;
+        if( p != nullptr ) {
+            auto left_result  = traverse_dump( p->left,  depth + 1 );
+            result.insert( result.end( ), left_result.begin( ), left_result.end( ) );
+            result.push_back( { depth, p->data } );
+            auto right_result = traverse_dump( p->right, depth + 1 );
+            result.insert( result.end( ), right_result.begin( ), right_result.end( ) );
+        }
+        return result;
     }
 
 }
--- SplayTree/SplayTree_demo.cpp
+++ SplayTree/SplayTree_demo.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 #include "SplayTree.hpp"
 
 using namespace std;
@@ -13,17 +14,51 @@ using namespace std;
 void simple_constructor_check( )
 {
     spica::SplayTree<int> tree1;
+    tree1.check_structure( );
 }
-
 
 void initializer_list_constructor_check( )
 {
 }
 
+void insert_check( )
+{
+    std::vector<int> test_data = { 5, 3, 7, 4, 6, 2, 8 };
+    vector<spica::SplayTree<int>::DumpResult> expected_dump_results = {
+         { { 0, 5 } },
+         { { 0, 3 }, { 1, 5 } },
+         { { 2, 3 }, { 1, 5 }, { 0, 7 } },
+         { { 1, 3 }, { 0, 4 }, { 2, 5 }, { 1, 7 } },
+         { { 2, 3 }, { 1, 4 }, { 2, 5 }, { 0, 6 }, { 1, 7 } },
+         { { 0, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 1, 6 }, { 2, 7 } },
+         { { 1, 2 }, { 4, 3 }, { 5, 4 }, { 6, 5 }, { 3, 6 }, { 2, 7 }, { 0, 8 } }
+    };
+
+    spica::SplayTree<int> tree1;
+    for( int i = 0; i < test_data.size( ); ++i ) {
+        tree1.insert( test_data[i] );
+        tree1.check_structure( );
+
+        spica::SplayTree<int>::DumpResult result = tree1.dump( );
+        if( result != expected_dump_results[i] ) {
+            cout << "Dump result mismatch at step " << i << endl;
+            cout << "Expected: " << endl;
+            for( const auto &dump_item : expected_dump_results[i] ) {
+                cout << "  " << "{ " << dump_item.depth << ", " << dump_item.data << " }" << endl;
+            }
+            cout << "Got: " << endl;
+            for( const auto &dump_item : result ) {
+                cout << "  " << "{ " << dump_item.depth << ", " << dump_item.data << " }" << endl;
+            }
+            exit( EXIT_FAILURE );
+        }
+    }
+}
 
 int main( )
 {
     simple_constructor_check( );
     initializer_list_constructor_check( );
+    insert_check( );
     return EXIT_SUCCESS;
 }
